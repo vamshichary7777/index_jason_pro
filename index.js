55ef6addf61449dc459d7a46081534a431ebba27
vamshichary7777 <119879631+vamshichary7777@users.noreply.github.com>
2023-09-14 17:38:46 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+exports.handler = async (event) => {
+  try {
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify('Hello, World!'),
+    };
+    return response;
+  } catch (error) {
+    console.error('Error:', error);
+    const response = {
+      statusCode: 500,
+      body: JSON.stringify('An error occurred'),
+    };
+    return response;
+  }
+};
